cc9f84500c98a7d1997db079f33c5ff29c0cad30
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-05 11:32:21 +0100

    configure db connection

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -2,15 +2,14 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGO_URI!);
     const connection = mongoose.connection;
 
     connection.on("connected", () => {
       console.log("MongoDB connected");
     });
     connection.on("error", (err) => {
-      console.log("MongoDB not connected", +err);
-      process.exit();
+      console.log("MongoDB not connected", err);
     });
   } catch (error) {
     console.log("Something went wrong");
