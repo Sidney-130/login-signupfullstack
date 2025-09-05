5be45594c0dbde92171bf7a0cb124761c8e185e1
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-05 11:32:00 +0100

    create signup rote

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -1,7 +1,7 @@
 import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextResponse, NextRequest } from "next/server";
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 
 connect();
 
@@ -20,8 +20,26 @@ export async function POST(req: NextRequest) {
         { status: 400 }
       );
     }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    return NextResponse.json({
+      message: "User created succesfully",
+      success: true,
+      savedUser,
+    });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
-    console.log();
+    console.log("Error");
   }
 }
