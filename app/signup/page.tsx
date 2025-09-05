0a52b3bd863c6429753983e4d3f706e35b5eb0fc
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-05 11:32:34 +0100

    add looading state

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,22 +1,50 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-// import { axios } from "axios";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 export default function Signup() {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabled, setButtondisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post("/api/users/signup", user);
+      console.log("Signup succesfull", res.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtondisabled(false);
+    } else {
+      setButtondisabled(true);
+    }
+  }, [user]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900 px-6">
-      <div className="flex flex-col w-full max-w-md bg-gray-800 p-8 rounded-2xl shadow-lg gap-4">
+    <div className="flex items-center justify-center min-h-screen bg-gray-900/10 px-6">
+      <div className="flex flex-col w-full max-w-md bg-gray-800/90 p-8 rounded-2xl shadow-lg gap-4">
         <h1 className="text-3xl font-bold tracking-wide text-left text-emerald-400 mb-4">
           Signup
         </h1>
@@ -61,7 +89,8 @@ export default function Signup() {
           className="w-full mt-4 bg-emerald-600 hover:bg-emerald-700 transition"
           onClick={onSignup}
         >
-          Sign Up
+          {buttonDisabled ? "No signup" : "Signup"}
+          {loading ? "..." : ""}
         </Button>
 
         <Link
